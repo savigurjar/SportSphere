c9f55bb6e2ae6f3a5e7316bddc8928504b254c43
savigurjar <savigurjar1201@gmail.com>
2026-03-11 12:25:02 +0530

    final deploy

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -7,8 +7,25 @@ const redisClient = require("./config/redis");
 const authRouter = require("./routes/userRoute")
 const cors = require("cors");
 
+// app.use(cors({
+//   origin: "http://localhost:5173",
+//   credentials: true,
+// }));
+const allowedOrigins = [
+  "https://himalayanfootballclub.onrender.com",
+  "http://localhost:5173",
+];
+
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true); // allow server-to-server / Postman
+
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    }
+
+    return callback(null, false); // IMPORTANT: don't throw
+  },
   credentials: true,
 }));
 
