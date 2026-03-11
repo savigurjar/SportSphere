30bec6949eed16c0ed6c96af7e8ace767c1ff411
savigurjar <savigurjar1201@gmail.com>
2026-03-11 12:11:15 +0530

    backend url added

--- frontend/src/utils/axiosClient.js
+++ frontend/src/utils/axiosClient.js
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
+const baseURL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:4000"
+    : "https://sportsphere-backend-ksch.onrender.com";
+
 const axiosClient = axios.create({
-  baseURL: "http://localhost:4000" || "https://sportsphere-backend-ksch.onrender.com",
+  baseURL,
   withCredentials: true,
   headers: { 'Content-Type': 'application/json' },
 });
